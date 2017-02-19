4c212280224dbecc8eab00942379657a875e4a33
bassaer <app.nakayama@gmail.com>
2017-02-19 20:59:11 +0900

    Replace library module

---
+++ app/src/main/java/com/github/bassaer/materialdesigncolors/ColorListAdapter.java
@@ -0,0 +1,7 @@
+package com.github.bassaer.materialdesigncolors;
+
+/**
+ * Created by nakayama on 2017/02/19.
+ */
+public class ColorListAdapter {
+}
---
+++ materialdesigncolors/src/main/java/com/github/bassaer/materialdesigncolors/MDColor.java
@@ -0,0 +1,7 @@
+package com.github.bassaer.materialdesigncolors;
+
+/**
+ * Created by nakayama on 2017/02/18.
+ */
+public class MDColor {
+}
